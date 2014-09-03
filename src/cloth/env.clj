08a8b566857e141100a31150cd84a643c1f66c06
Kristoffer Ström <kristoffer@rymdkoloni.se>
2014-09-04 00:06:00 +0200

    fixed broken reference

--- src/cloth/env.clj
+++ src/cloth/env.clj
@@ -4,14 +4,14 @@
 (defn create-contract [env code]
   (let [address        (hash code)
         [env executed] (vm/run env address 
-                               (vm/init-execution-state code))        
+                               (vm/init-state code))        
         new-env        (-> env 
                            (assoc-in [:contracts address :code]
                                      (vm/get-return-value executed)))]
     [new-env address]))
 
 (defn transaction [env to & args]
-  (let [state          (apply vm/init-execution-state
+  (let [state          (apply vm/init-state
                               (get-in env [:contracts to :code])
                               args)
         [env executed] (vm/run env to state)]
